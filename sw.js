547a8a20d6180d49f6c3b2a3861f678a98826e31
Yassinebouaida <70096826+Yassinebouaida@users.noreply.github.com>
2026-04-07 17:24:27 +0100

    Update service worker for PWA caching strategy

--- sw.js
+++ sw.js
@@ -1,18 +1,30 @@
-const CACHE_NAME = 'temu-v5';
-const FILES = [
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
+const CACHE_NAME = 'temu-pwa-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/style.css',
+    '/script.js',
+    '/manifest.json',
+    'https://unpkg.com/dexie@3.2.4/dist/dexie.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css'
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES).catch(console.warn)));
-  self.skipWaiting();
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request).catch(() => caches.match('./index.html')))
-  );
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => response || fetch(event.request))
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys => Promise.all(keys.map(key => {
+            if(key !== CACHE_NAME) return caches.delete(key);
+        })))
+    );
 });
