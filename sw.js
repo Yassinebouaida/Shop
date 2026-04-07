b0da723b787a578b95b818be10973089cd340026
Yassinebouaida <70096826+Yassinebouaida@users.noreply.github.com>
2026-04-07 16:59:50 +0100

    Add service worker for caching files

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'temu-v5';
+const FILES = [
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES).catch(console.warn)));
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request).catch(() => caches.match('./index.html')))
+  );
+});
